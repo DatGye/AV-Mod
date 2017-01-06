2cd4deb0182d949ef02ff38bf954de887baa1c72
Wyatt Phillips <datscigye@gmail.com>
2017-01-05 20:23:41 -0500

    Added initialization code and test entities

---
+++ lua/autorun/av_init.lua
@@ -0,0 +1,2 @@
+print("AV Mod is Running!")
+// include("entities/av_camera.lua")
---
+++ lua/entities/av_switch_in2.lua
@@ -0,0 +1,38 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_entity"
+ENT.PrintName = "Video Switcher (2 Inputs)"
+ENT.Spawnable = true
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+if SERVER then
+	
+	function ENT:Initialize()
+	
+		self:SetModel("models/dav0r/camera.mdl")
+		self:PhysicsInit(SOLID_VPHYSICS)
+		self:SetMoveType(MOVETYPE_VPHYSICS)
+		self:SetSolid(SOLID_VPHYSICS)
+		
+		local phys = self:GetPhysicsObject()
+		
+		if phys:IsValid() then
+		
+				phys:Wake()
+				
+		end
+		
+	end
+	
+end
+
+if CLIENT then
+
+	function ENT:Draw()
+	
+		self:DrawModel()
+	
+	end
+
+end
