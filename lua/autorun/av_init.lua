359fae41694a03f3a3dc674dec2d9713ea9cefc7
Wyatt Phillips <datscigye@gmail.com>
2017-01-16 20:25:42 -0500

    Implemented autorun script handler

--- lua/autorun/av_init.lua
+++ lua/autorun/av_init.lua
@@ -1,2 +1,35 @@
-print("AV Mod is Running!")
-// include("entities/av_camera.lua")
+--[[
+AV Mod (Ver. 1.0)
+Author: DatGye (Wyatt Phillips)
+
+Copyright 2016 Rogavox Development
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]--
+
+-- lua script handler (sets files to be run by client and server)
+
+if (SERVER) then
+	-- send scripts to client
+	AddCSLuaFile()
+	
+	-- run serverside scripts
+	
+end
+
+if (CLIENT) then
+	-- run clientside scripts
+	
+end
+
+-- run shared scripts
